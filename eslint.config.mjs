ad1a1608d06788b86b79412a447e763f29198354
Rishabh Tripathi <rishabh.j.tripathi2903@gmail.com>
2026-06-02 11:20:45 +0530

    chore: setup eslint, prettier, and code formatting rules

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "public/**",
+      "out/**",
+      "build/**",
+      "data/**",
+      "next-env.d.ts",
+    ],
+  },
+]);
+
+export default eslintConfig;
